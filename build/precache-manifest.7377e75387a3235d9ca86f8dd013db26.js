44eef2ca738ebc146713f5f99c58e69664576f61
Kalpesh <grimreaper1234566@gmail.com>
2020-07-04 16:33:56 +0530

    Add files via upload

---
+++ build/precache-manifest.7377e75387a3235d9ca86f8dd013db26.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "756f2d725cb09c0585ed4b86559ecca5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eefeb2abd7d5f1ab52af",
+    "url": "/static/css/main.f65227a3.chunk.css"
+  },
+  {
+    "revision": "03a6a31cb3ca6ee03382",
+    "url": "/static/js/2.bc20c50c.chunk.js"
+  },
+  {
+    "revision": "eefeb2abd7d5f1ab52af",
+    "url": "/static/js/main.34868590.chunk.js"
+  },
+  {
+    "revision": "92f416338fa5b92447d6",
+    "url": "/static/js/runtime-main.79fbf0b5.js"
+  }
+]);
---
+++ src/components/layout/Alert.js
@@ -0,0 +1,22 @@
+import React, {useContext} from 'react';
+import alertContext from '../../context/alert/alertContext';
+
+const Alert = () => {
+    const AlertContext = useContext(alertContext);
+
+    const { alert } = AlertContext;
+
+    return (
+        alert !== null && (
+            <div className={`alert alert-${alert.type}`}>
+                <i className="fas fa-info-circle">
+                    {alert.msg}
+                </i>
+            </div>
+        )
+    );
+}
+
+export default Alert;
+
+
